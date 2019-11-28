78750b57049cea2cff56df045cf253ef6db6c4d1
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-11-28 15:07:18 +0100

    Added mongo db connection

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // Configure dotenv for pulling sensitive data from .env file
 dotenv.config({ path: './config.env' });
 
 const app = require('./server/app');
+
+// Get password for deployment env
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
